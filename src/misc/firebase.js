fd28aafa675152d78673b84a09368b7b9a084ea1
Islamshaban98 <islamshaban998@gmail.com>
2020-11-17 09:43:14 +0200

    initialize firebase

---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyA2r8cMVcimypkFhu7EHQeG5qXXx15wFbc',
+  authDomain: 'chat-web-app-3a029.firebaseapp.com',
+  databaseURL: 'https://chat-web-app-3a029.firebaseio.com',
+  projectId: 'chat-web-app-3a029',
+  storageBucket: 'chat-web-app-3a029.appspot.com',
+  messagingSenderId: '988873562018',
+  appId: '1:988873562018:web:e04f2b41d3122d3c9f84fb',
+};
+
+firebase.initializeApp(config);
