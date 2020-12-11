f8382eef0b7ac0daff4875162b8fe99607e9d2c0
Islamshaban98 <islamshaban998@gmail.com>
2020-12-12 01:59:37 +0200

    adding chat bottom  section and display the last message

--- src/components/channels/Channel.js
+++ src/components/channels/Channel.js
@@ -1,20 +1,41 @@
 import React from 'react';
 import TimeAgo from 'timeago-react';
 import { hashName } from '../../misc/helperFunctions';
+import ProfileImage from '../profileImage';
 
 export default function Channel({ channel }) {
-  const { name, createdAt } = channel;
+  const { name, createdAt, lastMessage } = channel;
   return (
     <div>
       <div className="d-flex justify-content-between align-items-center mt-1">
         <h6 className="font-family-roboto"># {hashName(name)}</h6>
         <TimeAgo
-          datetime={new Date(createdAt)}
-          className="font-normal text-black-45"
+          datetime={
+            lastMessage ? new Date(lastMessage.createdAt) : new Date(createdAt)
+          }
+          className="font-family-roboto text-black-45"
         />
       </div>
       <div className="d-flex align-items-center text-black-45">
-        <span className="font-family-roboto">no messages yet ...</span>
+        {lastMessage ? (
+          <>
+            <div className="d-flex align-items-center mt-2">
+              <ProfileImage
+                src={lastMessage.author.avatar}
+                name={lastMessage.author.name}
+                size="sm"
+              />
+            </div>
+            <div className="text-disappear ml-2 mt-2">
+              <div className="font-family-roboto">
+                {lastMessage.author.name}
+              </div>
+              <span className="font-family-roboto">{lastMessage.message}</span>
+            </div>
+          </>
+        ) : (
+          <span className="font-family-roboto">no messages yet ...</span>
+        )}
       </div>
     </div>
   );
--- src/components/chat/bottom/index.js
+++ src/components/chat/bottom/index.js
@@ -1,4 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Alert, Icon, Input, InputGroup } from 'rsuite';
+import firebase from 'firebase/app';
+import { useParams } from 'react-router';
+import { useCurrentChannel } from '../../../context/currentChannelContext';
+import { hashName } from '../../../misc/helperFunctions';
+import { useProfile } from '../../../context/ProfileContext';
+import { database } from '../../../misc/firebase';
 
-const Bottom = () => <div>hello from bottom</div>;
+function assembleMessage(profile, channelId) {
+  return {
+    channelId,
+    author: {
+      name: profile.name,
+      uid: profile.uid,
+      // createdAt: profile.createdAt,
+      ...(profile.avatar ? { avatar: profile.avatar } : {}),
+    },
+    createdAt: firebase.database.ServerValue.TIMESTAMP,
+  };
+}
+
+const Bottom = () => {
+  const [input, setInput] = useState('');
+  const [isLoading, setisLoading] = useState(false);
+  const { profile } = useProfile();
+  const { channelId } = useParams();
+  const name = useCurrentChannel(v => v.name);
+  const onChange = value => {
+    setInput(value);
+  };
+  const onClick = async () => {
+    if (input.trim() === '') return;
+    const messageData = assembleMessage(profile, channelId);
+    messageData.message = input;
+
+    const update = {};
+    const messageId = database.ref('messages').push().key;
+    update[`/messages/${messageId}`] = messageData;
+    update[`/channels/${channelId}/lastMessage`] = {
+      ...messageData,
+      messageId,
+    };
+    setisLoading(true);
+    try {
+      await database.ref().update(update);
+      setInput('');
+      setisLoading(false);
+    } catch (err) {
+      setisLoading(false);
+      Alert.error(err.message, 4000);
+    }
+  };
+
+  const onKeyDown = e => {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      onClick();
+    }
+  };
+  return (
+    <div>
+      <InputGroup>
+        <Input
+          placeholder={`Message # ${hashName(name)}`}
+          value={input}
+          onChange={onChange}
+          onKeyDown={onKeyDown}
+        />
+        <InputGroup.Button
+          color="blue"
+          appearance="primary"
+          onClick={onClick}
+          disabled={isLoading}
+        >
+          <Icon icon="send" />
+        </InputGroup.Button>
+      </InputGroup>
+    </div>
+  );
+};
 export default Bottom;
